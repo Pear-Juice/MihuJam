74b9d58033b996fa2e55c33ca80853421f75e9df
Axel <axel_vera@live.com>
2024-06-14 20:04:04 -0400

    Dashing implemented
    - Added dash
    - Added more input mapping
    - Esc to unlock cursor
    - Click to lock cursor

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,27 +1,61 @@
 extends CharacterBody3D
 
-
+#Public variables
 @export var MovementSpeed : float
 @export var CameraSensitivity : float
+@export var DashSpeedOverTime : Curve
+@export var DashTime: float 
+@export var DashCooldown : float
 
+#Private variables
+var isDashing : bool = false
+var dashTimeElapsed : float = 0
+var currentSpeed : float
+
+#Components
 @onready var cam = $Camera3D
-#const JUMP_VELOCITY = 4.5
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	
-func _unhandled_input(event):
-	if event is InputEventMouseMotion:
-		rotate_y(-event.relative.x * CameraSensitivity)
-		cam.rotate_x(-event.relative.y * CameraSensitivity)
-		print("Mouse moved")
+	currentSpeed = MovementSpeed
 
 func _physics_process(delta):
 	#Handle movement
 	var input_dir = Input.get_vector("Left", "Right", "Forward", "Back")
 	velocity = (input_dir.y * cam.global_transform.basis.z) + (input_dir.x * global_transform.basis.x)
-	velocity *= MovementSpeed
+	velocity *= currentSpeed
 
 	move_and_slide()
+	
+func _process(delta):
+	if isDashing:
+		currentSpeed = DashSpeedOverTime.sample(dashTimeElapsed/DashTime)
+		dashTimeElapsed += delta
+		
+		
+		if dashTimeElapsed >= DashTime:
+			EndDash()
+	
+func _input(event):
+	if event.is_action_pressed("Pause"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		
+	if event.is_action_pressed("Dash") && !isDashing:
+		StartDash()
+	
+	if event.is_action_pressed("ItemUse_Left"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * CameraSensitivity)
+		cam.rotate_x(-event.relative.y * CameraSensitivity)
+
+
+func StartDash():
+	isDashing = true
+	dashTimeElapsed = 0
+
+func EndDash():
+	isDashing = false
+	currentSpeed = MovementSpeed
