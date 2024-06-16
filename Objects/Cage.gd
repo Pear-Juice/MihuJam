add02b47728796cd40757b6757cc07d01978d5e4
Sophia Scott <53879861+Pear-Juice@users.noreply.github.com>
2024-06-16 16:55:57 -0600

    Added music and spot player sound effects

--- Objects/Cage.gd
+++ Objects/Cage.gd
@@ -2,6 +2,8 @@ class_name Cage
 
 extends Node3D
 
+@export var end_music_curve : Curve
+
 var player_in_cage := true
 var treasure_in_cage := false
 var num_shark_in_cage := 0
@@ -39,8 +41,9 @@ func _on_door_on_close():
 	if player_in_cage:
 		player_is_safe = true
 		player_reached_safety.emit()
-	if num_shark_in_cage == 1:
-		print("shark ending")
+	
+	if num_shark_in_cage == 4:
+		leave()
 	if treasure_in_cage:
 		leave()
 
@@ -52,8 +55,14 @@ func _on_door_on_open():
 func leave():
 	await get_tree().create_timer(0.5).timeout
 	
-	get_tree().create_tween().set_ease(Tween.EASE_IN).tween_property(self, "global_position:y", 150, 5)
-	await get_tree().create_timer(3).timeout
+	Player.I.currentOxygen = Player.I.MaxOxygenTime
+	Player.I.end_player.play(11)
+	get_tree().create_tween().set_ease(Tween.EASE_IN).tween_property(Player.I.end_player, "volume_db", -10, 4)
+	get_tree().create_tween().set_ease(Tween.EASE_IN).tween_property(self, "global_position:y", 450, 21)
+	
+	var world = get_tree().root.get_child(0).get_child(0) as WorldEnvironment
+	get_tree().create_tween().tween_property(world.environment, "fog_light_color", Color("5186ff"), 7)
+	await get_tree().create_timer(7).timeout
+	Player.I.bootup_player.play()
 	
 	Player.I.win_game()
-	
