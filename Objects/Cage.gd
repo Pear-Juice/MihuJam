800b80242d0c55f024673dca346627351f287076
Sophia Scott <53879861+Pear-Juice@users.noreply.github.com>
2024-06-16 01:03:56 -0600

    Added worm shark, updated player death screen, added player death sounds,

--- Objects/Cage.gd
+++ Objects/Cage.gd
@@ -3,6 +3,7 @@ class_name Cage
 extends Node3D
 
 var player_in_cage := true
+var num_shark_in_cage := 0
 var player_is_safe := true
 
 signal player_reached_safety
@@ -16,20 +17,27 @@ func _init():
 	I = self
 
 func _on_area_3d_body_entered(body):
+	print(body, )
 	if body is Player:
 		player_in_cage = true
-		print("enter cage")
+	if body is Shark:
+		num_shark_in_cage += 1
+		print("shark enter")
 
 func _on_area_3d_body_exited(body):
+	print(body, "leave")
 	if body is Player:
 		player_in_cage = false
-		print("leave cage")
-
+	if body is Shark:
+		num_shark_in_cage -= 1
+		print("shark leave")
 
 func _on_door_on_close():
 	if player_in_cage:
 		player_is_safe = true
 		player_reached_safety.emit()
+	if num_shark_in_cage == 1:
+		print("shark ending")
 
 
 func _on_door_on_open():
