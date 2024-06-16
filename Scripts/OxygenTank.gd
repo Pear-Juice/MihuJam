6be660b74234a9bde98ba2a89241d5405b6f6f5e
Axel <axel_vera@live.com>
2024-06-16 06:29:38 -0400

    Added oxygen tank sfx

--- Scripts/OxygenTank.gd
+++ Scripts/OxygenTank.gd
@@ -1,7 +1,10 @@
 extends Node3D
 
+@onready var audio : AudioStreamPlayer3D = $AudioStreamPlayer3D
+
 @export var OxygenRefillAmount : float
 @export var RefillRate : float
+
 
 var player : Node3D
 var isInUse : bool = false
@@ -27,7 +30,9 @@ func OnItemUse():
 	if oxygenRemaining > 0:
 		isInUse = true
 		player.DepleteOxygen = false
+		audio.play(0)
 
 func OnCancelUse():
 	isInUse = false
 	player.DepleteOxygen = true
+	audio.stop()
